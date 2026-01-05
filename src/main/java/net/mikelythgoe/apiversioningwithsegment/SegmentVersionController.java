c88af935ec102b47c718886bafb5086a3c850ea7
mikelythgoe <github@mikelythgoe.net>
2026-01-05 08:41:28 +0000

    More examples of versioned endpoints with different formats (1.0,3.5, 9, 9.9)

--- src/main/java/net/mikelythgoe/apiversioningwithsegment/SegmentVersionController.java
+++ src/main/java/net/mikelythgoe/apiversioningwithsegment/SegmentVersionController.java
@@ -7,23 +7,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class SegmentVersionController {
 
     @GetMapping(value = "/{version}/hello", version = "1.0")
-    public String helloV1() {
+    public String helloV1point0() {
         return "Hello Version One";
     }
 
     // Version must be major.minor.patch format
     @GetMapping(value = "/{version}/hello", version = "2.0")
-    public String helloV2() {
+    public String helloV2point0() {
         return "Hello Version Two";
     }
 
     @GetMapping(value = "/{version}/hello", version = "3.5")
-    public String hellov35() {
+    public String hellov3point5() {
         return "Hello Version 3.5";
     }
 
+    // http://localhost:8080/9/hello
+    @GetMapping(value = "/{version}/hello", version = "9")
+    public String hellov9() {
+        return "Hello Version 9";
+    }
+
+    // http://localhost:8080/9.9/hello
     @GetMapping(value = "/{version}/hello", version = "9.9")
-    public String hellov99() {
+    public String hellov9point9() {
         return "Hello Version 9.9";
     }
+
 }

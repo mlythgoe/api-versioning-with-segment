f77c13a820ecc19c3aef26def75a6936bc90aba0
mlythgoe <github@mikelythgoe.net>
2026-01-05 19:19:42 +0000

    tweaked method names

--- src/main/java/net/mikelythgoe/apiversioningwithsegment/SegmentVersionController.java
+++ src/main/java/net/mikelythgoe/apiversioningwithsegment/SegmentVersionController.java
@@ -22,19 +22,19 @@ public class SegmentVersionController {
 
     // http://localhost:8080/3.5/hello (you can use /3.5/, or /3.5.0/, but you can't use /3.6., /3.5.1/, etc.)
     @GetMapping(value = "/{version}/hello", version = "3.5")
-    public String hellov3point5() {
+    public String helloV3point5() {
         return "Hello Version 3.5";
     }
 
     // http://localhost:8080/9/hello ( you can use /9/, /9.0/, or /9.0.0/, but you can't use /9.0.1/)
     @GetMapping(value = "/{version}/hello", version = "9")
-    public String hellov9() {
+    public String helloV9() {
         return "Hello Version 9";
     }
 
     // http://localhost:8080/9.9/hello
     @GetMapping(value = "/{version}/hello", version = "9.9")
-    public String hellov9point9() {
+    public String helloV9point9() {
         return "Hello Version 9.9";
     }
 
